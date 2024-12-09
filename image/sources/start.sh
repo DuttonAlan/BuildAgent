78ff2332244e94572e9795db93425ba21725f529
Alan <alan.dutton@plan-b-gmbh.com>
2024-12-09 16:09:50 +0100

    something changed

--- image/sources/start.sh
+++ image/sources/start.sh
@@ -98,7 +98,6 @@ trap 'cleanup; exit 143' TERM
 
 chmod +x ./run.sh
 
-
 # If $AZP_PLACEHOLDER is set, skipping running the agent
 if [ -n "$AZP_PLACEHOLDER" ]; then
   echo 'Running in placeholder mode, skipping running the agent'
